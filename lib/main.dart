b68bada6bb445916894d4e489314eb34057ff137
maramrmilad7 <maro12787@gmail.com>
2025-07-01 08:37:35 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FashoinApp());
+}
+
+class FashoinApp extends StatelessWidget {
+  const FashoinApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
